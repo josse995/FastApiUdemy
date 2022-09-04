bb7709b155a109817ea12b8f9a9977534aea7a92
josse995 <josse995@gmail.com>
2022-09-04 11:42:49 +0200

    Added env variable for Heroku's PostgreSQL

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #SQLALCHEMY_DATABASE_URL="sqlite:///./todos.db"
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:postgres:admin@localhost/TodoApplicationDatabase"
+#SQLALCHEMY_DATABASE_URL="postgresql://postgres:postgres:admin@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL=os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine= create_engine(
     #SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False}
