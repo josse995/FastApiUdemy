b7eb73a11089ff6d84c6a24f49dc160260914f27
josse995 <josse995@gmail.com>
2022-09-04 11:03:35 +0200

    Edit user password module added

--- routers/users.py
+++ routers/users.py
@@ -2,16 +2,23 @@ import sys
 sys.path.append("../TodoApp")
 
 import models
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request, Form
 from .auth import get_db, get_current_user, verify_password, get_password_hash, get_user_exception
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from database import engine
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette import status
+from starlette.responses import RedirectResponse
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
+    responses={404:{"detail": "Not found"}}
 )
+
+templates = Jinja2Templates(directory="templates")
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -81,6 +88,35 @@ def delete_current_user(user: dict = Depends(get_current_user), db: Session = De
     db.commit()
 
     return successful_response(200)
+
+@router.get("/edit-password", responses=HTMLResponse)
+async def edit_user_view(request: Request):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    return templates.TemplateResponse("edit-user-password.html", {"request": request, "user": user})
+
+@router.post("/edit-password", responses=HTMLResponse)
+async def user_password_change(request: Request, username: str = Form(...), password: str = Form(...),
+                               password2: str = Form(...), db: Session = Depends(get_db)):
+
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+
+    user_data = db.query(models.Users).filter(models.Users.username == username).first()
+
+    msg = "Invalid username or password"
+
+    if user_data is not None:
+        if username == user_data.username and verify_password(password, user_data.hashed_password):
+            user_data.hashed_password = get_password_hash(password2)
+            db.add(user_data)
+            db.commit()
+            msg = "Password updated"
+
+    return templates.TemplateResponse("edit-user-password.html", {"request": request, "user": user, "msg": msg})
+
 
 def successful_response(status_code: int):
     return {
